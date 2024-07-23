bd4975b9cc91fea15d69d922b93e73470cd3f1c5
Akinola Daniel <vantablack294@gmail.com>
2024-07-23 14:13:52 +0100

    update form

--- main.py
+++ main.py
@@ -17,20 +17,31 @@ with open("model.pkl", "rb") as f:
 
 @app.get("/", response_class=HTMLResponse)
 def index(request: Request, prediction_str: str = " "):
-    return templates.TemplateResponse("index.html", {"request": request, "name": "Hello world!", "prediction_str": prediction_str})
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request, "name": "Hello world!", "prediction_str": prediction_str},
+    )
 
 
 @app.post("/predict")
-def predict(request: Request, sepal_length: float = Form(...), sepal_width: float = Form(...), petal_length: float = Form(...), petal_width: float = Form(...)):
-    input_features = np.array(
-        [[sepal_length, sepal_width, petal_length, petal_width]])
+def predict(
+    request: Request,
+    sepal_length: float = Form(),
+    sepal_width: float = Form(),
+    petal_length: float = Form(),
+    petal_width: float = Form(),
+):
+    input_features = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
     preds = model.predict(input_features)
 
     species_map = {0: "Iris Setosa", 1: "Iris Versicolor", 2: "Iris Virginica"}
     predicted_species = species_map.get(preds[0])
-    return templates.TemplateResponse("index.html", {"request": request, "prediction_str": predicted_species})
+    return templates.TemplateResponse(
+        "index.html", {"request": request, "prediction_str": predicted_species}
+    )
 
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="127.0.0.1", port=8000)
